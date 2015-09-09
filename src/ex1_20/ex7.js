4f2c37a8a182bb42efa59638013d700d866bc47d
David Michael Gang <michaelgang@gmail.com>
2015-09-09 11:32:51 +0300

    exercise 7

---
+++ src/ex1_20/ex7.js
@@ -0,0 +1,22 @@
+import {primeGenerator}   from "../lib/numUtils";
+
+function nthPrime(n) {
+    let i = 1;
+    let gen = primeGenerator();
+    while(i < n) {
+        gen.next();
+        i++;
+    }
+    return gen.next().value;
+}
+
+export default class ex6 {
+    runTest() {
+        return nthPrime(6);
+    }
+
+    runEx() {
+        return nthPrime(10001);
+    }
+
+}
